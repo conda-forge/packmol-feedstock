3ac9ebfb7d6f44929d5650ac67073a57bbda8cd0
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-04-07 11:53:45 +0200

    let FFLAGS govern the process

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i.bak 's|FORTRAN=/usr/bin/gfortran|FORTRAN=${GFORTRAN}|g' Makefile
-sed -i.bak 's|FLAGS= |FLAGS= ${FFLAGS}|' Makefile
+sed -i.bak 's|FLAGS=.*|FLAGS= ${FFLAGS} -O3|' Makefile
 
 make
 mkdir -p ${PREFIX}/bin
