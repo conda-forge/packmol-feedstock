034b921e7a39a63f06d2648413fbb90600c9a031
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2024-08-16 15:00:31 -0700

    remove debuging stuff

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,21 +3,13 @@
 set -euxo pipefail
 
 rm -rf build || true
-
 mkdir build
 cd build
-
 cmake ${SRC_DIR} ${CMAKE_ARGS}
-
 make
 make install
-
 cd ../
+
 # copy tests 
 mkdir -p ${PREFIX}/share/packmol/tests
-echo cwd
-pwd
-ls -l
-echo "up one"
-ls -l ../
 cp -r testing/*  ${PREFIX}/share/packmol/tests
