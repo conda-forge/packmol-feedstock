7fdb1880a98f9af424552932020ddbc53c8d6901
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2024-08-19 09:29:56 -0700

    add more testing (#26)
    * first pass at adding tests
    
    * patch tests to use packmol from path
    
    * missed a hardcoded path
    
    * remove debuging stuff
    
    * forgot to bump build number
    
    * fix lint

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -euxo pipefail
+
 rm -rf build || true
 mkdir build
 cd build
-
 cmake ${SRC_DIR} ${CMAKE_ARGS}
-
 make
 make install
+cd ../
+
+# copy tests 
+mkdir -p ${PREFIX}/share/packmol/tests
+cp -r testing/*  ${PREFIX}/share/packmol/tests
